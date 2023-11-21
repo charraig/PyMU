19754ab4f5bc04cc19bcc487f7fa1f8cffaa84b4
charraig <meredith.miller10@gmail.com>
2023-11-21 14:44:53 -0500

    fixed crc imports

--- setup.py
+++ setup.py
@@ -47,8 +47,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=["pythoncrc"],
-    intall_requires=["pycrc"],
+    install_requires=["pythoncrc"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
